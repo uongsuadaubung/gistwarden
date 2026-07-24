e60acc41b86391c92578b49cc9fa802273c98b27
uongsuadaubung <manhkien13041997@gmail.com>
2026-07-24 10:53:38 +0700

    fix(build): bump Firefox strict_min_version to 142.0 to resolve data_collection_permissions warning

--- build.ts
+++ build.ts
@@ -84,7 +84,7 @@ function copyAssets() {
           manifest.browser_specific_settings = {
             gecko: {
               id: `${appNameLower}@uongsuadaubung.github.io`,
-              strict_min_version: "120.0",
+              strict_min_version: "142.0",
               data_collection_permissions: {
                 required: ["none"],
               },
